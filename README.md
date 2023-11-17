271383ee6a4f23509fb149eb4b7905d0bc788eb2
Douglas <dodo_nadoti@hotmail.com>
2023-11-17 15:41:03 -0300

    modificação no readme

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 Para acessar o projeto, faça um clone do repositorio no vscode, quando o repositorio estiver baixado, inicie com pnpm install.
 
-no arquivo api.ts dentro da pasta services, implemente seu token do github seguindo esse tutorial -[https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens], pois sem o token, o github da apenas um pequeno número de acesso pela API.
-
-Quando estiver acabado de instalar as dependências e colocado o token, inicie com pnpm dev.
+Quando estiver acabado de instalar as dependências, inicie com pnpm dev.
 
 <img src="/src/assets/avatar.png" alt="Texto Alternativo">
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,40 +1,24 @@
 import axios from "axios";
 const BASE_URL = "https://api.github.com";
 
-const token = "seu token aqui"
 
 export const getUser = (username: string) => {
   const url = `${BASE_URL}/users/${username}`;
   return axios.get(
-    url, 
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
+    url
   );
 };
 
 export const getUserRepository = (username: string) => {
   const url = `${BASE_URL}/users/${username}/repos`;
   return axios.get(
-    url, 
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
+    url
   );
 };
 
 export const getUserRepositoryDatails = (fullName: string) => {
   const url = `${BASE_URL}/repos/${fullName}`;
   return axios.get(
-    url, 
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
+    url
   );
 };
